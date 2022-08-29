e53a967f005959ef155c0e7f4e8259071349c7b4
Andreas <alstroem1992@gmail.com>
2022-08-29 19:22:04 +0200

    timber logging

--- app/src/main/java/dk/alstroem/showcase/ShowcaseApplication.kt
+++ app/src/main/java/dk/alstroem/showcase/ShowcaseApplication.kt
@@ -2,10 +2,14 @@ package dk.alstroem.showcase
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class ShowcaseApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 }
